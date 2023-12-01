406b6225ab5a81151b9f12425d13a6accafc6a3f
KashnykovAnton <80004672+KashnykovAnton@users.noreply.github.com>
2023-12-01 15:33:10 +0100

    Update 01-load-more.js

--- src/js/01-load-more.js
+++ src/js/01-load-more.js
@@ -33,8 +33,8 @@ function onClickButton(e) {
   checkEmptyValue() && fetchData(page, value);
 }
 
-function fetchData(page, value) {
-  fetchImages(page, value)
+async function fetchData(page, value) {
+  await fetchImages(page, value)
     .then(data => {
       createMarkup(data);
     })
